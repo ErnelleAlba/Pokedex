10df5c0676e5e1828a522f486e1a41fb48893bfb
John Ernelle Alba <johnernellealbaw@gmail.com>
2023-07-25 22:52:45 +0800

    :bento:Added logo and api url

---
+++ src/api/PokemonApi.jsx
@@ -0,0 +1,3 @@
+export const PokemonApiBaseUrl = "https://pokeapi.co/api/v2/"
+export const PokemonApiPokemonUrl = `${PokemonApiBaseUrl}pokemon`
+export const PokemonImagesBaseUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/"
